68a20e0cc3983ddc896be48f33ef354866cdaf1a
Stefan Eng <eng_steff@outlook.com>
2020-09-27 19:53:53 +0200

    Possible to toggle debug prints on and off.

--- lib/utils.h
+++ lib/utils.h
@@ -7,11 +7,17 @@
 #include <time.h>
 
 #define UNUSED(x) (void)x
+#define DEBUG 1
 
 void
 printf_stamped(const char * prefix, const char * fmt, ...);
 
-#define debug(fmt, ...) printf_stamped("D", fmt, __VA_ARGS__)
-#define error(fmt, ...) printf_stamped("!", fmt, __VA_ARGS__)
 
+#if DEBUG == 1
+  #define debug(fmt, ...) printf_stamped("D", fmt, __VA_ARGS__)
+#else
+  #define debug(fmt, ...) (void)0
+#endif
+
+#define error(fmt, ...) printf_stamped("!", fmt, __VA_ARGS__)
 #endif
